90e8d2efb361f5cfa0c49112feb7b4ad065ec775
Hawleywoo <alhawley21@gmail.com>
2020-12-12 23:19:22 -0700

    Webpack

--- server/index.jsx
+++ server/index.jsx
@@ -5,7 +5,10 @@ import fs from 'fs-extra';
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.get(['/'], function * (req,res) => {
+app.get(['/'], function * (req,res){
     let index = yield fs.readFile('./public/index.html', "utf-8");
     res.send(index);
 });
+
+
+app.listen(port, '0.0.0.0', ()=>console.info(`App listening on ${port}`));
