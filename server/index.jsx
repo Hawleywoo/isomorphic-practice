b0ee49d55c965c98a5e86e63600d310cd3d2be1a
Hawleywoo <alhawley21@gmail.com>
2020-12-15 22:02:39 -0700

    Webpack

--- server/index.jsx
+++ server/index.jsx
@@ -1,6 +1,30 @@
 import express from 'express';
 import yields from 'express-yields';
 import fs from 'fs-extra';
+import webpack from 'webpack';
+import { argv } from 'optimist';
+import { get } from 'request-promise';
+import { delay } from 'redux-saga'
+import { questions, question } from '../data'
+
+const useLiveData = argv.useLiveData === 'true'
+
+function * getQuestions(){
+    let data;
+    if(useLiveData){
+        data = yield get(questions, {gzip:true})
+    }
+}
+if(process.env.NODE_ENV === 'development'){
+    const config = require('../webpack.config.dev.babel').default;
+    const cmpiler = webpack(config);
+
+    app.use(require('webpack-dev-middleware')(compiler, {
+        noInfo:true
+    }));
+
+    app.use(require('webpack-hot-middleware')(compiler))
+}
 
 const port = process.env.PORT || 3000;
 const app = express();
