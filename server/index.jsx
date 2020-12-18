f728a0a478e1d01749a87995dbd029388b275eb8
Hawleywoo <alhawley21@gmail.com>
2020-12-17 18:53:00 -0700

    Redux saga and React

--- server/index.jsx
+++ server/index.jsx
@@ -13,8 +13,37 @@ function * getQuestions(){
     let data;
     if(useLiveData){
         data = yield get(questions, {gzip:true})
+    } else {
+        data = yield fs.readFile('./data/mock-questions.json','utf-8')
     }
+
+    return JSON.parse(data)
 }
+function * getQuestion(question_id){
+    let data;
+    if(useLiveData){
+        data = yield get (question(question_id), {gzip:true, json:true});
+    } else {
+        const questions = yield getQuestions();
+        const question = questions.items.find(_question=>_question.id == question_id)
+        question.body = `Mock question body: $ ${question_id}`;
+        data = {items:[question]}
+    }
+    return data;
+}
+
+app.get('api/questions', function  * (req, res){
+    const data = yield getQuestions();
+    yield delay(150);
+    res.json(data);
+})
+
+app.get('/api/questions/:id', function * (req, res){
+    const data = yield getQuestion(req.param.id);
+    yield delay(150);
+    res.json(data);
+}); 
+
 if(process.env.NODE_ENV === 'development'){
     const config = require('../webpack.config.dev.babel').default;
     const cmpiler = webpack(config);
